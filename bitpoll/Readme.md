88a33ac0c9da22106de9d049f9ec9ef76f6fc421
Chessmasterrr <webmaster@psdatatech.de>
2019-12-18 23:05:46 +0100

    added uwsgi for productive use

--- bitpoll/Readme.md
+++ bitpoll/Readme.md
@@ -1,3 +1,17 @@
-Bitpoll is a software to conduct polls about Dates, Times or general Questions.
+[Bitpoll](https://github.com/fsinfuhh/Bitpoll) is a software to conduct polls about Dates, Times or general Questions.
 
 The Dockerfile contains a Bitpoll installation.
+
+Download [settings_local.py](https://raw.githubusercontent.com/fsinfuhh/Bitpoll/master/bitpoll/settings_local.sample.py) and adjust the settings. An working example can be found [here](https://github.com/Chessmasterrr/docker/tree/master/bitpoll/settings_local.py).
+
+Run the image with:
+
+```bash
+docker run -p 8000:8000 -v /path/on/host/settings_local.py:/Bitpoll/bitpoll/settings_local.py -v /path/on/host/database/:/Bitpoll/database/ -d chessmasterrr/bitpoll:latest
+```
+
+On the first run, initialise the database by running (this will delete existing data!):
+
+```bash
+docker exec <containerID> ./manage.py migrate
+```
---
+++ bitpoll/settings_local.py
@@ -0,0 +1,109 @@
+# customize to your needs
+
+# You must insert your own random value here
+# SECURITY WARNING: keep the secret key used in production secret!
+# see <https://docs.djangoproject.com/en/dev/howto/deployment/checklist/#secret-key>
+SECRET_KEY = '123456789'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+## If Bitpoll is served via HTTPS enable the next two options
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+
+#Add additionall installed apps here
+## Example for installed raven (Sentry instrumentation)
+#INSTALLED_APPS_LOCAL = [
+#        'raven.contrib.django.raven_compat',
+#        ]
+INSTALLED_APPS_LOCAL = []
+
+# Compress the JS and CSS files, for more Options see https://django-pipeline.readthedocs.io/en/latest/compressors.html
+# the Compressor have to be installed in the system
+PIPELINE_LOCAL = {}
+#PIPELINE_LOCAL['JS_COMPRESSOR'] = 'pipeline.compressors.uglifyjs.UglifyJSCompressor'
+#PIPELINE_LOCAL['CSS_COMPRESSOR'] = 'pipeline.compressors.cssmin.CSSMinCompressor'
+#PIPELINE_ENABLED = True
+
+LANGUAGE_CODE = 'de-de'
+TIME_ZONE = 'Europe/Berlin'
+
+## https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/Bitpoll/database/db.sqlite3',
+    }
+}
+
+## Customize your instance
+SITE_NAME = 'example site'
+BASE_URL = 'https://example.de'
+
+## Url to the Base Homepage and Text on the Link, leave empty to not use this option
+HOME_URL = "https://example.de"
+HOME_URL_NAME = "Home"
+
+## Test mail functionality by printing mails to console:
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'example.de'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'user'
+EMAIL_HOST_PASSWORD = '1234'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'noreply@example.de'
+MAIL_SIGNATURE = 'Example-Team'
+
+## if the imprint URL is not empty use it as an link to the imprint, else use IMPRINT_TEXT
+IMPRINT_URL = "https://example.de/imprint"
+#IMPRINT_TEXT = """
+#<h1>ImpressuXm</h1>
+#<p>Text goes here</p>
+#"""
+
+#LOCALE_PATHS = (os.path.join(ROOT_DIR, 'locale'), )
+#LANGUAGES = (
+#    ('de', 'Deutsch'),
+#    ('en', 'English'),
+#    #('fr', 'Français'),
+#)
+
+REGISTER_ENABLED = True
+#GROUP_MANAGEMENT = REGISTER_ENABLED
+
+## Use ldap login
+#import ldap
+#from django_auth_ldap.config import LDAPSearch
+#
+#AUTHENTICATION_BACKENDS = (
+#    'django_auth_ldap.backend.LDAPBackend',
+#    'django.contrib.auth.backends.ModelBackend',
+#    )
+#
+#AUTH_LDAP_SERVER_URI = "ldap_host"
+#AUTH_LDAP_BIND_DN = "ldap_bind_dn"
+#AUTH_LDAP_BIND_PASSWORD = "ldap_bind_pw"
+#AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=People,dc=mafiasi,dc=de",
+#    ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
+#AUTH_LDAP_ALWAYS_UPDATE_USER = True
+#
+#from django_auth_ldap.config import LDAPSearch, PosixGroupType
+#
+#AUTH_LDAP_GROUP_SEARCH = LDAPSearch("ou=groups,dc=mafiasi,dc=de",
+#    ldap.SCOPE_SUBTREE, "(objectClass=posixGroup)"
+#    )
+#AUTH_LDAP_GROUP_TYPE = PosixGroupType()
+##AUTH_LDAP_FIND_GROUP_PERMS = True
+#AUTH_LDAP_MIRROR_GROUPS = True
+#
+#AUTH_LDAP_USER_ATTR_MAP = {"first_name": "givenName", "last_name": "sn", "email": "mail"}
+#
+#AUTH_LDAP_USER_FLAGS_BY_GROUP = {
+#    "is_staff": ["cn=Editoren,ou=groups,dc=mafiasi,dc=de",
+#                 "cn=Server-AG,ou=groups,dc=mafiasi,dc=de"],
+#    "is_superuser": "cn=Server-AG,ou=groups,dc=mafiasi,dc=de"
+#}
